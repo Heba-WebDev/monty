216f832803ab9ea331042d04a50a98bc607a2a1e
Heba Omar <jtvonline@yahoo.com>
2023-06-23 02:01:24 +0300

    Remove

--- main.c
+++ main.c
@@ -13,43 +13,5 @@
 int main(int argc, char **argv)
 {
 
-FILE *file = NULL;
-size_t buff_len = 0;
-char *buffer = NULL;
-char *op = NULL;
-stack_t *stack = NULL;
-unsigned int line_count = 1;
-int status = 0;
-if (argc != 2 || argv[1] == NULL)
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-file = fopen(argv[1], "r");
-if (file == NULL)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-free(file);
-exit(EXIT_FAILURE);
-}
-while (getline(&buffer, buff_len, file) != (-1))
-{
-if (status)
-break;
 
-if (*buffer == '\n')
-line_count++;
-continue;
-
-op = strtok(buffer, " \t\n");
-if (!op || *op == '#')
-line_count++;
-continue;
-
-global.argument = strtok(NULL, " \t\n");
-opcode(&stack, op, line_count);
-line_count++;
-}
-free(buffer);
-free_stack(stack);
-fclose(file);
-exit(EXIT_SUCCESS);
 }
--- pall.c
+++ pall.c
@@ -9,10 +9,5 @@
 
 void pall(stack_t **stack, unsigned int line_cnt)
 {
-if (!stack || !(*stack))
-{
-fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
-exit(EXIT_FAILURE);
-}
-printf("%d\n", (*stack)->n);
+
 }
